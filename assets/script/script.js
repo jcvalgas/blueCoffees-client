6f85a1eb070b262ec114d35a50e271698e089d76
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-04-23 11:59:39 -0300

    fixes with the database

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,93 +1,102 @@
-const baseURL = "http:///localhost:3002/coffees"
+const baseURL = 'http:///localhost:3002/coffees';
 
-async function findAllCoffees () {
-  const response = await fetch(`${baseURL}/find-coffees`)
+async function findAllCoffees() {
+  const response = await fetch(`${baseURL}/find-coffees`);
 
-  const coffees = await response.json()
-  coffees.forEach(function(element) {
-    document.querySelector('#CoffeeList').insertAdjacentHTML("beforeend",
+  const coffees = await response.json();
+  coffees.forEach(function (coffee) {
+    document.querySelector('#CoffeeList').insertAdjacentHTML(
+      'beforeend',
       `
-      <div class="CoffeeListItem" id="CoffeeListItem_${element.id}">
+      <div class="CoffeeListItem" id="CoffeeListItem_${coffee._id}">
         <div>
-          <div class="CoffeeListItem__sabor">${element.sabor}</div>
-          <div class="CoffeeListItem__preco">R$${element.preco}</div>
+          <div class="CoffeeListItem__sabor">${coffee.sabor}</div>
+          <div class="CoffeeListItem__preco">R$${coffee.preco}</div>
           <div class="CoffeeListItem__descricao">
-            ${element.descricao}
+            ${coffee.descricao}
           </div>
           <div class="Acoes">
-            <button class="Acoes__editar btn" onclick="abrirModal(${element.id})">Editar</button> 
-            <button class="Acoes__apagar btn" onclick="abrirModalDelete(${element.id})">Apagar</button>
+            <button class="Acoes__editar btn" onclick="abrirModal('${coffee._id}')">Editar</button> 
+            <button class="Acoes__apagar btn" onclick="abrirModalDelete('${coffee._id}')">Apagar</button>
           </div>
         </div>
-          <img class='CoffeeListItem__foto' src="${element.foto}" alt="${element.sabor}">    
+          <img class='CoffeeListItem__foto' src="${coffee.foto}" alt="${coffee.sabor}">    
       </div>
-    `)
-  })
+    `,
+    );
+  });
 }
 
-async function findByIdCoffees(){
-  const id = document.getElementById("idCoffee").value;
-  
+async function findByIdCoffees() {
+  const id = document.getElementById('idCoffee').value;
+
   const response = await fetch(`${baseURL}/find-coffees/${id}`);
-  
+
   const coffee = await response.json();
-  
-  const coffeeEscolhidoDiv = document.getElementById("CoffeeSelect");
-  
-  coffeeEscolhidoDiv.innerHTML = `<div class="CoffeeCardItem id="CoffeeListItem_${coffee.id}">
+
+  const coffeeEscolhidoDiv = document.getElementById('CoffeeSelect');
+
+  coffeeEscolhidoDiv.innerHTML = `<div class="CoffeeCardItem id="CoffeeListItem_${
+    coffee.id
+  }">
     <div>
       <div class="CoffeeCardItem__sabor">${coffee.sabor}</div>
-      <div class="CoffeeCardItem__preco">R$ ${coffee.preco.toFixed(2)}</div>
+      <div class="CoffeeCardItem__preco">R$ ${coffee.preco}</div>
       <div class="CoffeeCardItem__descricao">${coffee.descricao}</div>
       <div class="Acoes">
-        <button class="Acoes__editar btn" onclick="abrirModal(${coffee.id})">Editar</button> 
-        <button class="Acoes__apagar btn" onclick="abrirModalDelete(${coffee.id})">Apagar</button>
+        <button class="Acoes__editar btn" onclick="abrirModal('${
+          coffee._id
+        }')">Editar</button> 
+        <button class="Acoes__apagar btn" onclick="abrirModalDelete('${
+          coffee._id
+        }')">Apagar</button>
       </div>
     </div>
       <img class="PaletaCardItem__foto" src=${
         coffee.foto
       } alt=${`Paleta de ${coffee.sabor}`} />
     </div>`;
-};
+}
 
 findAllCoffees();
 
-async function abrirModal(id = null) {
-  if (id != null) {
-    document.querySelector("#title-header-modal").innerText = "Atualizar um Café";
-    document.querySelector("#button-form-modal").innerText = "Atualizar";
+async function abrirModal(id = '') {
+  if (id != '') {
+    document.querySelector('#title-header-modal').innerText =
+      'Atualizar um Café';
+    document.querySelector('#button-form-modal').innerText = 'Atualizar';
 
     const response = await fetch(`${baseURL}/find-coffees/${id}`);
     const coffee = await response.json();
 
-    document.querySelector("#sabor").value = coffee.sabor;
-    document.querySelector("#preco").value = coffee.preco;
-    document.querySelector("#descricao").value = coffee.descricao;
-    document.querySelector("#foto").value = coffee.foto;
-    document.querySelector("#id").value = coffee.id;
+    document.querySelector('#sabor').value = coffee.sabor;
+    document.querySelector('#preco').value = coffee.preco;
+    document.querySelector('#descricao').value = coffee.descricao;
+    document.querySelector('#foto').value = coffee.foto;
+    document.querySelector('#id').value = coffee._id;
   } else {
-    document.querySelector("#title-header-modal").innerText = "Cadastrar Café";
-    document.querySelector("#button-form-modal").innerText = "Cadastrar";
+    document.querySelector('#title-header-modal').innerText = 'Cadastrar Café';
+    document.querySelector('#button-form-modal').innerText = 'Cadastrar';
   }
 
-  document.querySelector(".modal-overlay").style.display = "flex";
+  document.querySelector('.modal-overlay').style.display = 'flex';
 }
 
 function fecharModal() {
-  document.querySelector(".modal-overlay").style.display = "none";
-  document.querySelector("#sabor").value = "";
-  document.querySelector("#preco").value = 0;
-  document.querySelector("#descricao").value = "";
-  document.querySelector("#foto").value = "";
+  document.querySelector('.modal-overlay').style.display = 'none';
+  document.querySelector('#sabor').value = '';
+  document.querySelector('#preco').value = 0;
+  document.querySelector('#descricao').value = '';
+  document.querySelector('#foto').value = '';
 }
 
-async function createCoffee() {
-  const id = document.getElementById("id").value;
-  const sabor = document.querySelector("#sabor").value;
-  const preco = document.querySelector("#preco").value;
-  const descricao = document.querySelector("#descricao").value;
-  const foto = document.querySelector("#foto").value;
-  console.log(id)
+async function submitCoffee() {
+  const id = document.getElementById('id').value;
+  const sabor = document.querySelector('#sabor').value;
+  const preco = document.querySelector('#preco').value;
+  const descricao = document.querySelector('#descricao').value;
+  const foto = document.querySelector('#foto').value;
+  console.log(id);
   const coffee = {
     id,
     sabor,
@@ -96,73 +105,57 @@ async function createCoffee() {
     foto,
   };
 
-  const modoEdicaoAtivado = id > 0;
+  const modoEdicaoAtivado = id != '';
 
-  const endpoint = baseURL + (modoEdicaoAtivado ? `/update/${id}` : `/create`)
+  const endpoint = baseURL + (modoEdicaoAtivado ? `/update/${id}` : `/create`);
 
   const response = await fetch(endpoint, {
     method: modoEdicaoAtivado ? `put` : `post`,
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    mode: "cors",
+    mode: 'cors',
     body: JSON.stringify(coffee),
   });
-  const newCoffee = await response.json()
-  console.log(newCoffee)
+  const newCoffee = await response.json();
 
-  const html = `<div class="CoffeeListItem" id="CoffeeListItem_${coffee.id}">
-  <div>
-    <div class="CoffeeListItem__sabor">${newCoffee.sabor}</div>
-    <div class="CoffeeListItem__preco">R$ ${newCoffee.preco}</div>
-    <div class="CoffeeListItem__descricao">${newCoffee.descricao}</div>
-    <div class="Acoes">
-      <button class="Acoes__editar btn" onclick="abrirModal(${newCoffee.id})">Editar</button> 
-      <button class="Acoes__apagar btn" onclick="abrirModalDelete(${newCoffee.id})">Apagar</button>
-    </div>
-  </div>
-    <img class="CoffeeListItem__foto" src=${
-      newCoffee.foto
-    } alt=${`Coffee de ${newCoffee.sabor}`} />
-  </div>`;
+  document.location.reload(true);
 
-  if(modoEdicaoAtivado) {
-    document.querySelector(`#CoffeeListItem_${id}`).outerHTML = html;
-  } else {
-    document.querySelector("#CoffeeList").insertAdjacentHTML("beforeend", html);
-  }
-  
-  fecharModal()
-};
+  // if(modoEdicaoAtivado) {
+  //   document.querySelector(`#CoffeeListItem_${id}`).outerHTML = html;
+  // } else {
+  //   document.querySelector("#CoffeeList").insertAdjacentHTML("beforeend", html);
+  // }
+
+  fecharModal();
+}
 
 function abrirModalDelete(id) {
-  document.querySelector("#overlay-delete").style.display = "flex";
+  document.querySelector('#overlay-delete').style.display = 'flex';
 
-  const btnSim = document.querySelector(".btn_delete_yes")
+  const btnSim = document.querySelector('.btn_delete_yes');
 
-  btnSim.addEventListener("click", function() {
+  btnSim.addEventListener('click', function () {
     deleteCoffee(id);
-  })
+  });
 }
 
 function fecharModalDelete() {
-  document.querySelector("#overlay-delete").style.display = "none";
+  document.querySelector('#overlay-delete').style.display = 'none';
 }
 
 async function deleteCoffee(id) {
   const response = await fetch(`${baseURL}/delete/${id}`, {
-    method: "delete",
+    method: 'delete',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    mode: "cors",
+    mode: 'cors',
   });
 
   const result = await response.json();
-  alert(result.message);
 
-  document.getElementById("CoffeeList").innerHTML = ""
+  document.location.reload(true);
 
   fecharModalDelete();
-  findAllCoffees();
 }
